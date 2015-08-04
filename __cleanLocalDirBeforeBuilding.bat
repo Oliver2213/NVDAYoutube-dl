20e45b262574f351bde7d0342da4b7d138ab96ab
Hrvoje Katić <hrvojekatic@gmail.com>
2015-08-04 12:39:20 +0200

    Fixed typo in my cleanup batch script.

--- __cleanLocalDirBeforeBuilding.bat
+++ __cleanLocalDirBeforeBuilding.bat
@@ -5,5 +5,5 @@ erase /S /Q *.dblite
 erase /S /Q *.ini
 erase /S /Q *.mo
 erase /S /Q *.html
-rd /S /Q .\addon\docs\en
-erase /Q .\addon\docs\*.css
+rd /S /Q .\addon\doc\en
+erase /Q .\addon\doc\*.css
